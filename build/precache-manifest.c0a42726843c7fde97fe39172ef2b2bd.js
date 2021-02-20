104101533bc0669821ea189f44c9b2998143c6f8
Quintis1212 <34488718+Quintis1212@users.noreply.github.com>
2021-02-20 19:10:49 +0200

    Add files via upload

---
+++ build/precache-manifest.c0a42726843c7fde97fe39172ef2b2bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61da9d68778a52c77bb483ec6a5caa5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "77d1855fad29a7e76b81",
+    "url": "./static/css/2.a67d35b8.chunk.css"
+  },
+  {
+    "revision": "d097e36321afc13e5252",
+    "url": "./static/css/main.a53f0919.chunk.css"
+  },
+  {
+    "revision": "77d1855fad29a7e76b81",
+    "url": "./static/js/2.8a1f1765.chunk.js"
+  },
+  {
+    "revision": "d2d428bc0aa7ba8f6d70655f3ad5b78a",
+    "url": "./static/js/2.8a1f1765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d097e36321afc13e5252",
+    "url": "./static/js/main.86e4e17a.chunk.js"
+  },
+  {
+    "revision": "60bcffd9386a21cd4be6",
+    "url": "./static/js/runtime-main.2ef5b252.js"
+  },
+  {
+    "revision": "a7d2ae7cdf035c26405090f388c7f9c4",
+    "url": "./static/media/home-page-img.a7d2ae7c.jpg"
+  },
+  {
+    "revision": "f12b7348ab3ab3b12736256c30e2ee0d",
+    "url": "./static/media/home-page-insta-img.f12b7348.jpg"
+  }
+]);
